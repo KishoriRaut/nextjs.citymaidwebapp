4da36b15e259281843cdd3a7e217879f4f0937d9
Your Name <your.email@example.com>
2025-05-30 04:56:08 -0700

    Update sitemap and SEO files

--- citymaidservices.net/next-sitemap.config.js
+++ citymaidservices.net/next-sitemap.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   siteUrl: 'https://www.citymaidservices.net',
   generateRobotsTxt: true,
-  sourceDir: 'app',
-  // exclude: ['/404', '/_error', '/_not-found'],
+  outDir: 'public',
 };
