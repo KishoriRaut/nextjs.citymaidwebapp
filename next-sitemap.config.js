5ba638075589085e7aed85219831d0557c756900
Your Name <your.email@example.com>
2025-05-29 21:13:52 -0700

    Move next-sitemap config to repo root and update build script for Vercel compatibility

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: 'https://www.citymaidservices.net',
+  generateRobotsTxt: true,
+  sourceDir: 'citymaidservices.net/app',
+  // exclude: ['/404', '/_error', '/_not-found'],
+};
