d0100e881dce9f389c96b8fdae1b4145a535e9d8
Your Name <your.email@example.com>
2025-05-29 21:26:31 -0700

    Set outDir for next-sitemap to fix build-manifest error on Vercel

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteUrl: 'https://www.citymaidservices.net',
   generateRobotsTxt: true,
-  sourceDir: 'citymaidservices.net/app',
+  outDir: 'citymaidservices.net/public',
   // exclude: ['/404', '/_error', '/_not-found'],
 };
