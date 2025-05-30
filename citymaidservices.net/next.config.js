c1a92e7390fdf300af12144d46a1ca34e95473c8
Your Name <your.email@example.com>
2025-05-29 21:32:00 -0700

    Set nextConfigPath for next-sitemap to detect Next.js build output in subdirectory

---
+++ citymaidservices.net/next.config.js
@@ -0,0 +1,2 @@
+// If you have custom Next.js config, put it here. Otherwise, this can be an empty file for next-sitemap to detect.
+module.exports = {};
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@ module.exports = {
   siteUrl: 'https://www.citymaidservices.net',
   generateRobotsTxt: true,
   outDir: 'citymaidservices.net/public',
+  nextConfigPath: 'citymaidservices.net/next.config.js',
   // exclude: ['/404', '/_error', '/_not-found'],
 };
